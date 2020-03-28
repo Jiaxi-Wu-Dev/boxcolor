de21c8f7abb73e602a6648c82d6ae3b752c35ae5
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-03-28 15:56:05 -0400

    got the counter of clicks to show on screen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+/* Objective
+
+Create a <div> box with an onClick handle event that changes the background color when clicked. 
+When the box is clicked, we want a clickCounter indicating how many times it was clicked. 
+We want the box color to toggle back to it's original color! (Hint: create a toggle function) */
+
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      color: 'blue',
+      clickCounter: 0
+    }
+  }
+
+  onChange = (e) => {
+    this.setState({
+      color: 'orange',
+      clickCounter: this.state.clickCounter + 1 
+    })
+    console.log("click counter ===> ", this.state.clickCounter)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> Box color change {this.state.clickCounter} </h1>
+        <div              className="colorBox"
+        style={{backgroundColor: this.state.color}}
+        onClick={this.onChange}
+        >
+        <p>Click Here</p>
+        </div>
+      </div>
+    )
+  }
+}
